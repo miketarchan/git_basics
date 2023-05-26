44d22ae586dea6163cd84e7d3e7cacb15b5d63a9
Michael Tarchan <muhasjo@gmail.com>
2023-05-26 23:39:49 +0300

    code cleanup

--- lesson-6/cards.py
+++ lesson-6/cards.py
@@ -12,7 +12,7 @@ _COLOR_RED = '\033[31m'
 _COLOR_BLACK = '\033[30m'
 _DEFAULT_COLOR = '\033[39m'
 
-# or ranks = list(range(6, 10 + 1)) + list('JQKA')
+
 _regular_suits = {
     'spades': SuitView(symbol='\u2660', color=_COLOR_RED),
     'hearts': SuitView(symbol='\u2665', color=_COLOR_RED),
@@ -20,15 +20,9 @@ _regular_suits = {
     'clubs': SuitView(symbol='\u2663', color=_COLOR_BLACK)
 }
 
-# class SuitEnum(Enum):
-#     def __str__(cls):
-#         return f'{cls.value}'
-    
-# Suit = SuitEnum('Suit', _regular_suits)
-
-
 class Card:
     __slots__ = 'rank', 'suit' # consume less memory
+
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
@@ -36,16 +30,10 @@ class Card:
     def __str__(self):
         return f'{self.suit.color} {self.rank} {self.suit.symbol}{_DEFAULT_COLOR}'
 
-## ranks = [6, 7, 8, 9, 10]
-## suits = ['clubs', 'spades', 'hearts', 'diamonds']
-## itertools.product(ranks, suits)
-# equivalent to:
-##for r in ranks:
-## for s in suits:
-## print(f'{r} : {s}')
 
 class CardDeckBase:
-
+    """Represent base logic for card deck"""
+    
     def getCardList(self,ranks, suits):
         result=[]
         for r in range(len(ranks)):
@@ -53,10 +41,6 @@ class CardDeckBase:
                 result.append(Card(ranks[r],suits[s]))
 
         return result
-
-    # def __str__(self) -> str:
-    #     pass
-    
 
 
 class RussianCardDeck(CardDeckBase):
@@ -70,7 +54,6 @@ class FrenchCardDeck(CardDeckBase):
     @property
     def cards(self):
         return super().getCardList(_additional_ranks + _regular_ranks , _regular_suits)
-
 
 if __name__ == '__main__':
     pass
